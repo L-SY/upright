db24f51835d3580c7d2389e0574c7166265ca6bc
LSY <2028238109@qq.com>
2024-03-12 14:41:27 +0800

    Add Cost.

--- upright_control/include/upright_control/cost/QuadraticJointStateInputCost.h
+++ upright_control/include/upright_control/cost/QuadraticJointStateInputCost.h
@@ -1,8 +1,53 @@
-//
-// Created by lsy on 24-3-12.
-//
+#pragma once
 
-#ifndef SRC_QUADRATICJOINTSTATEINPUTCOST_H
-#define SRC_QUADRATICJOINTSTATEINPUTCOST_H
+#include <ocs2_core/cost/QuadraticStateCost.h>
+#include <ocs2_core/cost/QuadraticStateInputCost.h>
+#include <ocs2_core/reference/TargetTrajectories.h>
 
-#endif //SRC_QUADRATICJOINTSTATEINPUTCOST_H
+namespace upright {
+
+    class QuadraticJointStateInputCost final
+            : public ocs2::QuadraticStateInputCost {
+    public:
+        explicit QuadraticJointStateInputCost(const MatXd& Q, const MatXd& R)
+                : ocs2::QuadraticStateInputCost(Q, R) {
+            xd_ = VecXd::Zero(Q.rows());
+        }
+
+        explicit QuadraticJointStateInputCost(const MatXd& Q, const MatXd& R,
+                                              const VecXd& xd)
+                : ocs2::QuadraticStateInputCost(Q, R), xd_(xd) {}
+
+        ~QuadraticJointStateInputCost() override = default;
+
+        QuadraticJointStateInputCost* clone() const override {
+            return new QuadraticJointStateInputCost(*this);
+        }
+
+        std::pair<VecXd, VecXd> getStateInputDeviation(
+                ocs2::scalar_t time, const VecXd& state, const VecXd& input,
+                const ocs2::TargetTrajectories& targetTrajectories) const override {
+            return {state - xd_, input};
+        }
+
+        VecXd xd_;
+    };
+
+    class QuadraticJointStateCost final : public ocs2::QuadraticStateCost {
+    public:
+        explicit QuadraticJointStateCost(const MatXd& Q)
+                : ocs2::QuadraticStateCost(Q) {}
+
+        ~QuadraticJointStateCost() override = default;
+
+        QuadraticJointStateCost* clone() const override {
+            return new QuadraticJointStateCost(*this);
+        }
+
+        VecXd getStateDeviation(ocs2::scalar_t time, const VecXd& state,
+                                const ocs2::TargetTrajectories&) const override {
+            return state;
+        }
+    };
+
+}  // namespace upright
