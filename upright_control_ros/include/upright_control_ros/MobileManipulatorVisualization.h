579d5242eed2ced0888444ee79ac734dc5c9b4c8
LSY <2028238109@qq.com>
2024-03-13 16:51:24 +0800

    A rough supplement to Vis.

--- upright_control_ros/include/upright_control_ros/MobileManipulatorVisualization.h
+++ upright_control_ros/include/upright_control_ros/MobileManipulatorVisualization.h
@@ -2,7 +2,48 @@
 // Created by lsy on 24-3-13.
 //
 
-#ifndef SRC_MOBILEMANIPULATORVISUALIZATION_H
-#define SRC_MOBILEMANIPULATORVISUALIZATION_H
+#pragma once
 
-#endif //SRC_MOBILEMANIPULATORVISUALIZATION_H
+#include <robot_state_publisher/robot_state_publisher.h>
+#include <tf/transform_broadcaster.h>
+
+#include <ocs2_ros_interfaces/mrt/DummyObserver.h>
+
+#include <ocs2_mobile_manipulator/ManipulatorModelInfo.h>
+#include <upright_control/MobileManipulatorInterface.h>
+#include <ocs2_self_collision_visualization/GeometryInterfaceVisualization.h>
+
+namespace ddt {
+
+    class MobileManipulatorDummyVisualization final : public ocs2::DummyObserver {
+    public:
+        MobileManipulatorDummyVisualization(ros::NodeHandle& nodeHandle, const upright::ControllerInterface& interface)
+                : pinocchioInterface_(interface.getPinocchioInterface()), modelInfo_(interface.getManipulatorModelInfo()) {
+            launchVisualizerNode(nodeHandle);
+        }
+
+        ~MobileManipulatorDummyVisualization() override = default;
+
+        void update(const ocs2::SystemObservation& observation, const ocs2::PrimalSolution& policy, const ocs2::CommandData& command) override;
+
+    private:
+        void launchVisualizerNode(ros::NodeHandle& nodeHandle);
+
+        void publishObservation(const ros::Time& timeStamp, const ocs2::SystemObservation& observation);
+        void publishTargetTrajectories(const ros::Time& timeStamp, const ocs2::TargetTrajectories& targetTrajectories);
+        void publishOptimizedTrajectory(const ros::Time& timeStamp, const ocs2::PrimalSolution& policy);
+
+        ocs2::PinocchioInterface pinocchioInterface_;
+        const ManipulatorModelInfo modelInfo_;
+        std::vector<std::string> removeJointNames_;
+
+        std::unique_ptr<robot_state_publisher::RobotStatePublisher> robotStatePublisherPtr_;
+        tf::TransformBroadcaster tfBroadcaster_;
+
+        ros::Publisher stateOptimizedPublisher_;
+        ros::Publisher stateOptimizedPosePublisher_;
+
+        std::unique_ptr<ocs2::GeometryInterfaceVisualization> geometryVisualization_;
+    };
+
+}  // namespace mobile_manipulator
