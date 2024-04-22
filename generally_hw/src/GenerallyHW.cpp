53617e61793e70b64e7433efe1e24500a374a070
LSY <2028238109@qq.com>
2024-04-22 11:04:48 +0800

    Add hybrid interface.

--- generally_hw/src/GenerallyHW.cpp
+++ generally_hw/src/GenerallyHW.cpp
@@ -4,26 +4,30 @@
 #include "generally_hw/GenerallyHW.h"
 
 namespace generally {
-    bool GenerallyHW::init(ros::NodeHandle& root_nh, ros::NodeHandle& /*robot_hw_nh*/) {
-        if (!loadUrdf(root_nh)) {
-            ROS_ERROR("Error occurred while setting up urdf");
-            return false;
-        }
+bool GenerallyHW::init(ros::NodeHandle &root_nh,
+                       ros::NodeHandle & /*robot_hw_nh*/) {
+  if (!loadUrdf(root_nh)) {
+    ROS_ERROR("Error occurred while setting up urdf");
+    return false;
+  }
 
-        registerInterface(&jointStateInterface_);
-        registerInterface(&effortJointInterface_);
-//        registerInterface(&imuSensorInterface_);
-        return true;
-    }
+  registerInterface(&jointStateInterface_);
+  registerInterface(&effortJointInterface_);
+  registerInterface(&positionJointInterface_);
+  registerInterface(&velocityJointInterface_);
+  registerInterface(&hybridJointInterface_);
+  //        registerInterface(&imuSensorInterface_);
+  return true;
+}
 
-    bool GenerallyHW::loadUrdf(ros::NodeHandle& rootNh) {
-        std::string urdfString;
-        if (urdfModel_ == nullptr) {
-            urdfModel_ = std::make_shared<urdf::Model>();
-        }
-        // get the urdf param on param server
-        rootNh.getParam("robot_description", urdfString);
-        return !urdfString.empty() && urdfModel_->initString(urdfString);
-    }
+bool GenerallyHW::loadUrdf(ros::NodeHandle &rootNh) {
+  std::string urdfString;
+  if (urdfModel_ == nullptr) {
+    urdfModel_ = std::make_shared<urdf::Model>();
+  }
+  // get the urdf param on param server
+  rootNh.getParam("robot_description", urdfString);
+  return !urdfString.empty() && urdfModel_->initString(urdfString);
+}
 
-}  // namespace legged
+} // namespace generally
